5286f2175780b7d5987f8fc3d017fbc426490d41
edward.zheng <edward.zheng@ringcentral.com>
2019-05-24 15:37:22 +0800

    fix bug of login

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,11 +3,18 @@
 public class Calculator {
 
     public int calculate(String operation, int i, int j) {
-        if(operation == "add") {
+        if (operation == "add") {
             return i + j;
         }
-        if(operation.equals("substract")) {
+        if (operation.equals("substract")) {
             return i - j;
+        }
+
+        if (operation.equals("divide")) {
+            if (j != 0) {
+                return i / j;
+            }
+            throw new IllegalArgumentException("j should not be 0");
         }
 
         throw new RuntimeException("Unsupported Operation" + operation);
